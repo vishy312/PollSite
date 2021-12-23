07ba65e029f8bf98bd2ae23e340a54ee289c0b71
vishal pal <vishalpal6486@gmail.com>
2021-12-23 21:26:28 +0530

    part-4 completed

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Choice
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Question, Choice
 
 
-def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')
-    return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:]
 
 
-def detail(request, question_id):
-    detail = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'detail': detail})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def result(request, question_id):
-    return HttpResponse("This is the result for question no. %s" % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You are voting for question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'error': "You didn't choose anything"})
+    else:
+        selected_choice.vote_tally += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+
